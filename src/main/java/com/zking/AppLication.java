251c5ea48af1c600a6fce0a5dc02918f0c155afd
kongsh <kongsuhongbaby@126.com>
2019-05-16 21:30:23 +0800

    修改登录文件

--- src/main/java/com/zking/AppLication.java
+++ src/main/java/com/zking/AppLication.java
@@ -3,7 +3,7 @@ package com.zking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 /**
  * Created by Administrator on 2017/5/5.
  */
